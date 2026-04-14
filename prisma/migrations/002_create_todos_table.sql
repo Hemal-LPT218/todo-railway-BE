96213052ef2f5fb4814e098e8b3456f29848e970
comet-conservation <github-comet@songswift.com>
2026-04-14 12:49:27 +0530

    Add migration

---
+++ prisma/migrations/002_create_todos_table.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `todos` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `text` TEXT NOT NULL,
+    `completed` BOOLEAN NOT NULL DEFAULT false,
+    `user_id` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    INDEX `todos_user_id_idx`(`user_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `todos` ADD CONSTRAINT `todos_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
