1d12800ba00d5cf50c292ddf053ce29912d410c6
comet-conservation <github-comet@songswift.com>
2026-04-14 17:21:47 +0530

    Update cors

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // 🔥 REQUIRED
+app.options(/.*/, cors());
 
 // Compression middleware
 app.use(compression());
